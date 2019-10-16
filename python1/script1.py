e750795346027742398dac46036fbe932a8351a3
info <info@info07.cshl.edu>
2019-10-16 11:16:50 -0400

    ok

--- python1/script1.py
+++ python1/script1.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
-my_name = 'Carla'
+import sys
 
-print(my_name)
+my_name = sys.argv[1]
 
+fav_color = sys.argv[2]
+
+fav_activity = sys.argv[3]
+
+fav_animal = sys.argv[4]
+
+print('My name: ' + my_name + '\n' +
+	'My favorite color: ' + fav_color + '\n' +
+	'My favorite activity: ' + fav_activity + '\n' +
+	'My favorite animal: ' + fav_animal)
