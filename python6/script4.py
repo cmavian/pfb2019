ec34ac14f3cfa9edb30426a716bc95d8660994c5
info <info@info07.cshl.edu>
2019-10-19 16:31:13 -0400

    script5 fastaparser

--- python6/script4.py
+++ python6/script4.py
@@ -6,17 +6,19 @@ import re
 fasta = sys.argv[1]
 
 fasta_dic = {}
-#pattern = r"gi"
 value = ''
 
 with open(fasta, 'r') as read_fasta:
 	for line in read_fasta:
 		line = line.rstrip()
 		if line.startswith('>'):# you find in the line starts with > assign the result to value of dictionary
+			value = ''
 			key = re.search(r'^>(\S+) (.\S.+)', line) # fasta_ID = '>'+key
 			geneID = key.group(1)
-			fasta_dic[geneID] = ''
-		else: # concatanate each line and assign to value
+			fasta_dic[geneID] = value
+			print(geneID)
+		else: # concatanate each other line and assign to value
 			value += line 
 			fasta_dic[geneID] = value
+
 print(fasta_dic)
--- python6/script5.py
+++ python6/script5.py
@@ -6,19 +6,19 @@ import re
 fasta = sys.argv[1]
 
 fasta_dic = {}
-#pattern = r"gi"
 value = ''
 
 with open(fasta, 'r') as read_fasta:
 	for line in read_fasta:
 		line = line.rstrip()
 		if line.startswith('>'):# you find in the line starts with > assign the result to value of dictionary
+			value = ''
 			key = re.search(r'^>(\S+) (.\S.+)', line) # fasta_ID = '>'+key
 			geneID = key.group(1)
-			fasta_dic[geneID] = ''
+			fasta_dic[geneID] = value
 			print(geneID)
-		else: # concatanate each line and assign to value
+		else: # concatanate each other line and assign to value
 			value += line 
 			fasta_dic[geneID] = value
 
-print(len(fasta_dic))
+print(fasta_dic)
